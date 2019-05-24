57dcef3597486abe617a2a1d8b3b29c72f14a220
aravintharajp <50869424+aravintharajp@users.noreply.github.com>
2019-05-24 07:22:45 +0530

    Update and rename README.md to a23.c

---
+++ a23.c
@@ -0,0 +1,26 @@
+int main()
+{
+    
+int arr[50],n,i,min;
+    
+scanf("%d",&n);
+    
+for(i=0;i<n;i++)
+        
+scanf("%d",&arr[i]);
+        
+min=arr[0];
+        
+for(i=1;i<n;i++)
+        
+{
+            
+if(min>arr[i])
+            
+min=arr[i];
+        
+}
+        
+printf("%d",min);
+
+}
